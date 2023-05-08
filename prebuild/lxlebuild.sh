a4070e8ccd6310dcfc4ad8b19e617b1680470040
DM Cyber Security <128435685+dmcys@users.noreply.github.com>
2023-05-08 17:19:23 -0400

    disabling all tty's

--- prebuild/lxlebuild.sh
+++ prebuild/lxlebuild.sh
@@ -1,4 +1,28 @@
 #!bin/bash
+tty1() {
+#!/bin/bash
+tty() {
+# Pick the default TTY used to open the graphical interface or user access
+read -p "Input your number of default TTY: " tty_atual
+
+# Loop through all ttys avaibles on >> /dev/tty*
+for tty in /dev/tty*
+do
+    # Extract the path complet to TTY 
+    tty_numero=${tty#/dev/tty}
+
+    # Disable all TTY encountered on folder
+    if [[ $tty_numero != $tty_atual ]]
+    then
+        sudo systemctl stop getty@$tty_numero.service
+        sudo systemctl disable getty@$tty_numero.service
+        echo "TTY $tty_numero disabled"
+    fi
+done
+}
+tty
+}
+
 sysctl() {
 echo "# Set the kernel panic timeout to 10 seconds
 kernel.panic=10
@@ -204,7 +228,7 @@ sudo apt -y install florence
 echo -ne -n -e "#!/usr/bin/env\n[Desktop Entry]\nType=Application\nName=Florence\nGenericName=VirtualKeyboard\nComment= To Start the floroence\nCategories=Keyboard\nExec=sh -c florence\nStartupWMClass=Florence\n" > ~/florence.desktop
 }
 
-
+tty1
 sysctl
 packages
 config
