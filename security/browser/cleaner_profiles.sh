37f97463641493a5207764970a3d6ac0bb8d34d7
DM Cyber Security <128435685+dmcys@users.noreply.github.com>
2023-05-13 14:51:49 +0000

    fixed to delete folders and files with different commands

--- security/browser/cleaner_profiles.sh
+++ security/browser/cleaner_profiles.sh
@@ -1,25 +1,28 @@
 #!bin/bash
+# Run this in the folder that have the profiles of browser
 cleanb() {
-# Put this file inside the dir that contain the profiles of browser
 find . -depth -name "cache2" -type d -exec rm -r {} \;
+find . -type f -name "storage-sync-v2.sqlite" -delete
+find . -type f -name "storage-sync-v2.sqlite-shm" -delete
+find . -type f -name "storage-sync-v2.sqlite-wal" -delete
 find . -depth -name "datareporting" -type d -exec rm -r {} \;
 find . -depth -name "safebrowsing" -type d -exec rm -r {} \;
 find . -depth -name "security_state" -type d -exec rm -r {} \;
 find . -depth -name "sessionstore-backups" -type d -exec rm -r {} \;
 find . -depth -name "startupCache" -type d -exec rm -r {} \;
 find . -depth -name "storage" -type d -exec rm -r {} \;
-find . -depth -name "addonStartup.json.lz4" -type d -exec rm -r {} \;
-find . -depth -name "cert9.db" -type d -exec rm -r {} \;
-find . -depth -name "content-prefs.sqlite" -type d -exec rm -r {} \;
-find . -depth -name "cookies.sqlite-wal" -type d -exec rm -r {} \;
-find . -depth -name "favicons.sqlite" -type d -exec rm -r {} \;
-find . -depth -name "formhistory.sqlite" -type d -exec rm -r {} \;
-find . -depth -name "key4.db" -type d -exec rm -r {} \;
-find . -depth -name "permissions.sqlite" -type d -exec rm -r {} \;
-find . -depth -name "places.sqlite" -type d -exec rm -r {} \;
-find . -depth -name "places.sqlite-wal" -type d -exec rm -r {} \;
-find . -depth -name "protections.sqlite" -type d -exec rm -r {} \;
-find . -depth -name "storage.sqlite" -type d -exec rm -r {} \;
-find . -depth -name "webappsstore.sqlite" -type d -exec rm -r {} \;
+find . -type f -name "addonStartup.json.lz4" -delete
+find . -type f -name "cert9.db" -delete
+find . -type f -name "content-prefs.sqlite" -delete
+find . -type f -name "cookies.sqlite-wal" -delete
+find . -type f -name "favicons.sqlite" -delete
+find . -type f -name "formhistory.sqlite" -delete
+find . -type f -name "key4.db" -delete
+find . -type f -name "permissions.sqlite" -delete
+find . -type f -name "places.sqlite" -delete
+find . -type f -name "places.sqlite-wal" -delete
+find . -type f -name "protections.sqlite" -delete
+find . -type f -name "storage.sqlite" -delete
+find . -type f -name "webappsstore.sqlite" -delete
 }
 cleanb
